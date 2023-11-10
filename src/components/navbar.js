08082a350f9c57131c93bacc41927cc6c5f59f80
SerlioGiron <serlio.giron@unitec.edu>
2023-11-10 13:57:41 -0600

    haciendo cambios en el nav bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -28,30 +28,26 @@ const Navbar = () => {
                                     Home
                                 </a>
                             </li>
-                            
-                            <li class="nav-item dropdown">
+                            <li class="nav-item">
                                 <a
-                                    class="nav-link dropdown-toggle"
+                                    class="nav-link active"
+                                    aria-current="page"
                                     href="#"
-                                    role="button"
-                                    data-bs-toggle="dropdown"
-                                    aria-expanded="false"
                                 >
-                                    Para padres
+                                    Soy Padre
                                 </a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a class="dropdown-item" href="#">
-                                            Action
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item" href="#">
-                                            Another action
-                                        </a>
-                                    </li>
-                                </ul>
                             </li>
+                            <li class="nav-item">
+                                <a
+                                    class="nav-link active"
+                                    aria-current="page"
+                                    href="#"
+                                >
+                                    Soy Estudiante
+                                </a>
+                            </li>
+                            
+                            
                         </ul>
                     </div>
                 </div>
